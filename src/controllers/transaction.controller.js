912270a02dc262533e7a88a572c423a65e3009b1
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-04-14 00:13:30 +0530

    feat: enhance transaction creation to include delay and update status handling

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -105,50 +105,71 @@ async function createTransaction(req, res) {
     });
   }
 
-  /**
-   * 5. Create transaction (PENDING)
-   */
-  const session = await mongoose.startSession();
-  session.startTransaction();
+  let transaction;
+  try {
+    /**
+     * 5. Create transaction (PENDING)
+     */
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
-  const transaction = new transactionModel({
-    fromAccount,
-    toAccount,
-    amount,
-    idempotencyKey,
-    status: "PENDING",
-  });
+    transaction = (
+      await transactionModel.create(
+        [
+          {
+            fromAccount,
+            toAccount,
+            amount,
+            idempotencyKey,
+            status: "PENDING",
+          },
+        ],
+        { session },
+      )
+    )[0];
 
-  const debitLedgerEntry = await ledgerModel.create(
-    [
-      {
-        account: fromAccount,
-        amount: amount,
-        transaction: transaction._id,
-        type: "DEBIT",
-      },
-    ],
-    { session },
-  );
+    const debitLedgerEntry = await ledgerModel.create(
+      [
+        {
+          account: fromAccount,
+          amount: amount,
+          transaction: transaction._id,
+          type: "DEBIT",
+        },
+      ],
+      { session },
+    );
 
-  const creditLedgerEntry = await ledgerModel.create(
-    [
-      {
-        account: toAccount,
-        amount: amount,
-        transaction: transaction._id,
-        type: "CREDIT",
-      },
-    ],
-    { session },
-  );
+    const creditLedgerEntry = await ledgerModel.create(
+      [
+        {
+          account: toAccount,
+          amount: amount,
+          transaction: transaction._id,
+          type: "CREDIT",
+        },
+      ],
+      { session },
+    );
 
-  transaction.status = "COMPLETED";
-  await transaction.save({ session });
+    await (() => {
+      return new Promise((resolve) => setTimeout(resolve, 15 * 1000));
+    })();
 
-  await session.commitTransaction();
-  session.endSession();
+    await transactionModel.findOneAndUpdate(
+      { _id: transaction._id },
+      { status: "COMPLETED" },
+      { session },
+    );
 
+    await session.commitTransaction();
+    session.endSession();
+  } catch (error) {
+    return res.status(400).json({
+      message:
+        "Transaction is Pending due to some issue, Please retry after sometime",
+    });
+  }
   /**
    * 10. Send email notification
    */
