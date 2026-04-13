a605e5db5b414bad846a0eda79d12e80fd99250f
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-04-14 00:56:02 +0530

    feat: add token blacklist model with expiration index

---
+++ src/models/blackList.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const tokenBlackListSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: [true, "Token is required to blackList"],
+      unique: [true, "Token is already blackListed"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+tokenBlackListSchema.index(
+  { blackListedAt: 1 },
+  {
+    expireAfterSeconds: 60 * 60 * 24 * 3, // 3 days
+  },
+);
+
+const tokenBlackListModel = mongoose.model(
+  "tokenBlackList",
+  tokenBlackListSchema,
+);
+
+module.exports = tokenBlackListModel;
